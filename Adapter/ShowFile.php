c88188b57cfac8db0a446ea22628ab55437b9641
yinm <yinm.cminor9@gmail.com>
2017-10-04 08:21:03 +0900

    Add class of use result

---
+++ Adapter/ShowFile.php
@@ -0,0 +1,26 @@
+<?php
+namespace PhpDesignPattern\Adapter;
+
+class ShowFile
+{
+    private $filename;
+
+    public function __construct($filename)
+    {
+        if (! is_readable($filename)) {
+            throw new \Exception('file "' . $filename . '" is not readable !');
+        }
+
+        $this->filename = $filename;
+    }
+
+    public function showPlain()
+    {
+        echo file_get_contents($this->filename);
+    }
+
+    public function showHighlight()
+    {
+        highlight_file($this->filename);
+    }
+}
