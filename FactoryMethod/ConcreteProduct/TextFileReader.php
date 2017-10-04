1190e0c01d9933c4d513c6e18b311889d08db00d
yinm <yinm.cminor9@gmail.com>
2017-10-05 08:46:53 +0900

    Add TextFileReader

---
+++ FactoryMethod/ConcreteProduct/TextFileReader.php
@@ -0,0 +1,62 @@
+<?php
+namespace PhpDesignPattern\FactoryMethod\ConcreteProduct;
+
+use PhpDesignPattern\FactoryMethod\Product\Reader;
+
+class TextFileReader implements Reader
+{
+    private $filename;
+    private $handler;
+
+    public function __construct($filename)
+    {
+        if (! is_readable($filename)) {
+            throw new \Exception('file [' . $filename . '] is not readable !');
+        }
+        $this->filename = $filename;
+    }
+
+    public function read()
+    {
+        $this->handler = fopen($this->filename, 'r');
+    }
+
+    public function display()
+    {
+        $prevArtist = null;
+        $titles = [];
+
+        while (($buffer = fgets($this->handler, 4096)) !== false) {
+            $data = explode("\t", trim($buffer));
+            if (count($data) !== 2) {
+                continue;
+            }
+
+            list($artist, $title) = $data;
+            if (is_null($prevArtist)) {
+                $prevArtist = $artist;
+            }
+            if ($artist !== $prevArtist) {
+                $this->displayDetail($prevArtist, $titles);
+
+                $prevArtist = $artist;
+                $titles = [];
+            }
+            $titles[] = $title;
+        }
+
+        if (count($titles) > 0) {
+            $this->displayDetail($prevArtist, $titles);
+        }
+    }
+
+    private function displayDetail($artist, array $titles = [])
+    {
+        printf('%s%s', $artist, PHP_EOL);
+        printf(
+            '-->%s%s',
+            implode(PHP_EOL . '-->', $titles),
+            count($titles) > 0 ? PHPP_EOL : null
+        );
+    }
+}
