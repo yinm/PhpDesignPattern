d056f6489e71660876d2c89f3d9d71989cb7c9d2
yinm <yinm.cminor9@gmail.com>
2017-10-06 05:43:47 +0900

    Add Facade class

---
+++ Facade/Facade/OrderManager.php
@@ -0,0 +1,19 @@
+<?php
+namespace PhpDesignPattern\Facade\Facade;
+
+use PhpDesignPattern\Facade\Subsystem\Order;
+use PhpDesignPattern\Facade\Subsystem\ItemDao;
+use PhpDesignPattern\Facade\Subsystem\OrderDao;
+
+class OrderManager
+{
+    public static function order(Order $order)
+    {
+        $itemDao = ItemDao::getInstance();
+        foreach ($order->getItems() as $orderItem) {
+            $itemDao->setAside($orderItem);
+        }
+
+        OrderDao::createOrder($order);
+    }
+}
