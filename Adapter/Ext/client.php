c0902935f07c128594c76bcff2715ec5a9b57d76
yinm <yinm.cminor9@gmail.com>
2017-10-10 07:57:48 +0900

    2nd

--- Adapter/Ext/client.php
+++ Adapter/Ext/client.php
@@ -5,5 +5,8 @@ require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
 use PhpDesignPattern\Adapter\Ext\Adapter\DisplaySourceFileImpl;
 
+/**
+ * 2nd
+ */
 $showFile = new DisplaySourceFileImpl(dirname(__DIR__) . '/ShowFile.php');
 $showFile->display();
