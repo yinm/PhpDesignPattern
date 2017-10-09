884aa057069e7d8d9db6135cc181c7233e76dbcd
yinm <yinm.cminor9@gmail.com>
2017-10-10 07:49:48 +0900

    2nd

--- Adapter/Ext/Target/DisplaySourceFile.php
+++ Adapter/Ext/Target/DisplaySourceFile.php
@@ -1,6 +1,12 @@
 <?php
 namespace PhpDesignPattern\Adapter\Ext\Target;
 
+/**
+ * 2nd
+ *
+ * Interface DisplaySourceFile
+ * @package PhpDesignPattern\Adapter\Ext\Target
+ */
 interface DisplaySourceFile
 {
     public function display();
