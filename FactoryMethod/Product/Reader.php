e25834671470cbabd6ddc22cf81f7af167f0be5c
yinm <yinm.cminor9@gmail.com>
2017-10-11 08:10:29 +0900

    2nd

--- FactoryMethod/Product/Reader.php
+++ FactoryMethod/Product/Reader.php
@@ -1,6 +1,12 @@
 <?php
 namespace PhpDesignPattern\FactoryMethod\Product;
 
+/**
+ * 2nd
+ *
+ * Interface Reader
+ * @package PhpDesignPattern\FactoryMethod\Product
+ */
 interface Reader
 {
     public function read();
