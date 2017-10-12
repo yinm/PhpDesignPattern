3a0c4099f2bd417e569e55faca077211ec0a2dae
yinm <yinm.cminor9@gmail.com>
2017-10-12 20:28:31 +0900

    2nd

--- Facade/Facade/OrderManager.php
+++ Facade/Facade/OrderManager.php
@@ -5,6 +5,12 @@ use PhpDesignPattern\Facade\Subsystem\Order;
 use PhpDesignPattern\Facade\Subsystem\ItemDao;
 use PhpDesignPattern\Facade\Subsystem\OrderDao;
 
+/**
+ * 2nd
+ *
+ * Class OrderManager
+ * @package PhpDesignPattern\Facade\Facade
+ */
 class OrderManager
 {
     public static function order(Order $order)
